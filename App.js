07cc186aba227b221c4b1e6be22c299b38ba2559
eduardolorcap-creator <eduardo.lorcap@csvp.cl>
2026-04-16 01:52:24 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text style={{fontSize:24, color:'#ff6a00'}}>
+        Agenda San Vicente
+      </Text>
+    </View>
+  );
+}
